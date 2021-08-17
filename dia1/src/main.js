68a93d4a6bfb56df8b61f0e8f62e1e406efd8686
Pedro Leão <phenriqueleao@gmail.com>
2021-08-17 11:00:12 -0300

    Exercício 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,12 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]')
+
+let isHidden = false;
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  isHidden = !isHidden
+  document.querySelector('[data-js="app"]').hidden = isHidden
+})
