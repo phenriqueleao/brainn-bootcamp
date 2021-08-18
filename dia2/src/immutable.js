a6528e02f187cba777fea269b66eb704f1618fdb
Pedro Leão <phenriqueleao@gmail.com>
2021-08-18 19:03:04 -0300

    Dia 2 - Exercício 1

---
+++ dia2/src/immutable.js
@@ -0,0 +1,20 @@
+const john = {
+  name: 'John',
+  surname: 'Doe',
+  age: 30,
+  hobbies: ['Surf', 'Design'],
+}
+
+// const jane = john
+
+// jane.name = 'Jane'
+// jane.hobbies.push('MuayThai', 'Programming')
+
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: [...john.hobbies, 'MuayThai', 'Programming']
+}
+
+console.log('John:', john)
+console.log('Jane:', jane)
